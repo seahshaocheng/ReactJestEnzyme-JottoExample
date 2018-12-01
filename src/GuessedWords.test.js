f9566b658ce471038d4ed9590100938b1a698dfa
Mark Seah <mark.seah@globalsignin.com>
2018-12-01 14:49:46 +0630

    41 Code for

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -28,7 +28,20 @@ test('does not throw warning with expected props',() => {
 })
 
 describe('if there are no words guessed',()=>{
+    let wrapper;
 
+    beforeEach (()=>{
+        wrapper = setUp({guessedWords:[]});
+    });
+
+    test('renders without error',()=>{
+        const component = findByTestAttr(wrapper,'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+    test('renders instructions to guess a word',()=>{
+        const instructions = findByTestAttr(wrapper, 'guess-instruction');
+        expect(instructions.text().length).not.toBe(0);
+    });
 });
 
 describe('if there are words guessed',()=>{
