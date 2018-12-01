5ed2f95df18683991b4381ef95f64a026468d611
Mark Seah <mark.seah@globalsignin.com>
2018-12-01 14:43:04 +0630

    38. Guessed words Prop Types and Set up

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {findByTestAttr,checkProps} from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+    guessedWords:[
+        {
+            guessedWords:'train',
+            letterMatchCount:3
+        }
+    ]
+};
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props
+ * @return {JSX.element} - Rendered component (Or null if `success` props)
+ */
+const setUp = (props={}) => {
+    const setupProps = {...defaultProps,...props}
+    return shallow(<GuessedWords {...setupProps}/>)
+}
+
+test('does not throw warning with expected props',() => {
+    checkProps(GuessedWords,defaultProps);
+})
